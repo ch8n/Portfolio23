3fbcd6f607f7308bfe1ea22726f0d702ce792925
ch8n <chetan.garg36@gmail.com>
2023-05-06 23:10:54 +0530

    added about/qualification

---
+++ src/components/about/Content.svelte
@@ -0,0 +1,23 @@
+<script>
+  import DiscTextList from "../primitives/DiscTextList.svelte";
+  import Title from "../primitives/Title.svelte";
+  export let sections = [
+    "Resume",
+    "Highlights",
+    "Contact",
+    "Connect",
+    "Work Experience",
+    "Qualification",
+    "Awards",
+    "Testimonials",
+    "About Website",
+  ];
+</script>
+
+<Title content="Content 📒" />
+<div class="flex-col mt-2 ms-4">
+  <DiscTextList textList={sections} />
+</div>
+
+<style>
+</style>
