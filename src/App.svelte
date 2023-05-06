6e7a04b4b2b202725663a595c372b92ae9ad5de8
ch8n <chetan.garg36@gmail.com>
2023-05-06 14:10:42 +0530

    added about/qualification

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,12 @@
 <script>
+  import Awards from "./components/about/Awards.svelte";
   import Connect from "./components/about/Connect.svelte";
   import Contact from "./components/about/Contact.svelte";
   import Highlights from "./components/about/Highlights.svelte";
   import Qualification from "./components/about/Qualification.svelte";
   import Resume from "./components/about/Resume.svelte";
+  import Testimonials from "./components/about/Testimonials.svelte";
+  import Website from "./components/about/Website.svelte";
   import WorkExperience from "./components/about/WorkExperience.svelte";
   import Spacer from "./components/primitives/Spacer.svelte";
 </script>
@@ -20,6 +23,12 @@
   <WorkExperience />
   <Spacer />
   <Qualification />
+  <Spacer />
+  <Awards />
+  <Spacer />
+  <Testimonials />
+  <Spacer />
+  <Website />
 </main>
 
 <style>
