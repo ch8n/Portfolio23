81bea5d1d61a5785b84a54b47ecce745c9cfbd2a
ch8n <chetan.garg36@gmail.com>
2023-09-18 18:23:25 +0530

    wip exp

--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -9,12 +9,12 @@
 <div class="grid grid-flow-col grid-rows-4 gap-4">
 	{#each experiences as experience}
 		<div class="p-8">
-			<p>{experience.designation}</p>
-			<p>{experience.company}</p>
-			<p>{experience.company_type}</p>
-			<p>{experience.location}</p>
-			<p>{experience.duration}</p>
-			<p>{experience.type}</p>
+			<p>Designation : {experience.designation}</p>
+			<p>Company : {experience.company}</p>
+			<p>Industry : {experience.industry}</p>
+			<p>Location : {experience.location}</p>
+			<p>Duration : {experience.duration}</p>
+			<p>Type : {experience.type}</p>
 		</div>
 	{/each}
 </div>
