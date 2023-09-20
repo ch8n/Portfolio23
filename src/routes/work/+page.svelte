b8d51d372cb9758af66ea2563b397417feda97c7
ch8n <chetan.garg36@gmail.com>
2023-09-20 22:02:15 +0530

    added work exp cards

--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -18,26 +18,44 @@
 	}
 </script>
 
-<div class="grid grid-rows-1 gap-6">
+<div class="grid grid-rows-1 grid-cols-1 gap-6">
+	<div class="absolute divide-x-2 divide-green-500" />
+
 	{#each experiences as experience}
-		<div
-			class={`relative overflow-clip w-11/12 p-8 bg-[${experience.style.bg_hex}] bg-opacity-70 rounded-tr-[36px] rounded-bl-[36px] rounded-br-[36px] shadow`}
-		>
-			<div class="absolute right-1 top-10 w-64 h-64 rounded-full z-0">
-				<img src={`./assets/${experience.style.icon_path}`} alt={experience.company} />
+		<div class="grid grid-cols-2 h-44">
+			<div class="col-span-1 relative">
+				<div class="flex z-10 absolute top-16 left-8">
+					<p class="place-self-center font-semibold px-2">2020</p>
+
+					<div
+						class={`w-12 h-12 ${experience.style.bg_color} rounded-full bottom-0 drop-shadow-lg`}
+					>
+						<img src={`./assets/${experience.style.icon_path}`} alt={experience.company} />
+					</div>
+				</div>
+
+				<div class="absolute z-0 left-28 -ml-0.5 w-0.5 h-screen bg-gray-600" />
 			</div>
 
-			<div class="relative">
-				<p class={`text-[${experience.style.font_hex}] text-3xl font-semibold`}>
-					{experience.designation}
-				</p>
-				<p class={`text-[${experience.style.font_hex}] text-xl font-semibold pt-2`}>
-					{experience.company} ({experience.industry})
-				</p>
-				<p class={`text-[${experience.style.font_hex}] text-sm font-semibold pt-2`}>
-					{experience.location}
-					{experience.duration}
-				</p>
+			<div
+				class={`col-span-2 start-20 place-self-center relative overflow-clip w-4/5 p-8  ${experience.style.bg_color} bg-opacity-95 rounded-tr-[36px] rounded-bl-[36px] rounded-br-[36px] drop-shadow-lg`}
+			>
+				<div class="absolute right-1 top-5 w-64 h-64 rounded-full z-0">
+					<img src={`./assets/${experience.style.icon_path}`} alt={experience.company} />
+				</div>
+
+				<div class={`relative ${experience.style.font_color}`}>
+					<p class={`text-3xl font-semibold pt-2`}>
+						{experience.designation}
+					</p>
+					<p class={`text-xl pt-2`}>
+						{experience.company} ({experience.industry})
+					</p>
+					<p class={`text-sm pt-2`}>
+						{experience.location}
+						{experience.duration}
+					</p>
+				</div>
 			</div>
 		</div>
 	{/each}
