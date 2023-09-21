cb45face17e9043b3f284b5f20ee343d5d879a0c
ch8n <chetan.garg36@gmail.com>
2023-09-21 15:55:50 +0530

    wip awards home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,12 +6,36 @@
 	}
 </script>
 
-<div class="p-12">
-	<div class="text-white text-3xl font-semibold" on:click={gotoAwards}>Awards 🏆</div>
+<div class="p-4">
+	<!-- header -->
+	<div class="grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-2 pt-8">
+		<img class={`col-span-1 mx-auto place-self-center`} src={`./assets/dev_pic.svg`} alt="" />
 
-	<p class="px-4 my-4 text-white text-sm font-normal">
-		You can see list of Awards I have won over my contibution in the communities and participation
-		into various challenges and events from
-		<span class="text-teal-300 text-sm font-semibold" on:click={gotoAwards}>here</span>
-	</p>
+		<ul class={`lg:col-span-5 md:col-span-3 sm:grid-cols-1 ms-8 p-8 text-xl list-disc`}>
+			<li>I’m <span class="font-semibold">Senior Software Engineer (Android)</span> from India</li>
+			<li>Currently working <span class="font-semibold">@Housing.com</span></li>
+			<li>Having <span class="font-semibold">5+ Years of experience</span></li>
+			<li><span class="font-semibold">Android development with Kotlin and Java</span></li>
+			<li>
+				Core team member @ <span class="font-semibold">Kotlin Mumbai</span> &
+				<span class="font-semibold">The Code Monk</span>
+			</li>
+			<li>
+				Technical Blogger @ <span class="font-semibold">Ex-Draft.dev</span>, Personally @
+				<span class="font-semibold">chetangupta.net</span>
+				and <span class="font-semibold">Medium</span>
+			</li>
+		</ul>
+	</div>
+
+	<!-- awards -->
+	<div class="mt-4">
+		<div class=" text-white text-3xl font-semibold" on:click={gotoAwards}>Awards 🏆</div>
+
+		<p class="px-4 my-4 text-white text-sm font-normal">
+			You can see list of Awards I have won over my contibution in the communities and participation
+			into various challenges and events from
+			<span class="text-teal-300 text-sm font-semibold" on:click={gotoAwards}>here</span>
+		</p>
+	</div>
 </div>
