7b78e1e4a5623162e7f2b28e15feed5316424a60
ch8n <chetan.garg36@gmail.com>
2024-05-24 13:05:30 +0530

    github action update

--- svelte.config.js
+++ svelte.config.js
@@ -10,13 +10,13 @@ const config = {
 	preprocess: [importAssets(), vitePreprocess()],
 	kit: {
 		adapter: adapter({
-			// default options are show n. On some platforms
+			// default options are shown. On some platforms
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
 			precompress: false,
 			strict: true,
-			fallback: '404.html'
+			fallback: ''
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
